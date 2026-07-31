59973a6b6d638a6818d02f26c34744cd62ddac69
Roshni Sahu <roshnisahu11222@gmail.com>
2026-07-31 23:46:53 +0530

    Time: 31 ms (83.12%), Space: 59 MB (86.27%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // If the smallest number is > 0, three positive numbers can't sum to 0
+            if (nums[i] > 0) break;
+            
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for the second element
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for the third element
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Sum is too small, move left pointer right to make it bigger
+                } else {
+                    right--; // Sum is too large, move right pointer left to make it smaller
+                }
+            }
+        }
+        
+        return result;
+    }
+}
