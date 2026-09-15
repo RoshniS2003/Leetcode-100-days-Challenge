6407783985684c1cd16c7bba6a56c62e3e3c20b2
Roshni Sahu <roshnisahu11222@gmail.com>
2026-09-15 23:46:40 +0530

    Time: 0 ms (100%), Space: 48.1 MB (46%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n; // Use long to handle integer overflow when n = Integer.MIN_VALUE
-        
-        // Handle negative exponent
+        long N = n;
         if (N < 0) {
             x = 1 / x;
             N = -N;
         }
-
-        double result = 1.0;
+        
+        double ans = 1.0;
         double currentProduct = x;
-
+        
         while (N > 0) {
-            // If N is odd, multiply the result by currentProduct
             if (N % 2 == 1) {
-                result *= currentProduct;
+                ans *= currentProduct;
             }
-            // Square the base and halve the exponent
             currentProduct *= currentProduct;
             N /= 2;
         }
-
-        return result;
+        
+        return ans;
     }
 }
